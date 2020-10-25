f3cb4def812260d5202ef3487092b81a1c79d79e
ihabbou <10790679+ihabbou@users.noreply.github.com>
2020-10-25 12:01:35 +0100

    Annotation converter: parse ICDAR xml

---
+++ data_utils/annotation_converter.py
@@ -0,0 +1,57 @@
+
+def parse_tables_from_xml(xml_path):
+    """Parses the ICDAR2019 xml annotation into a list of objects.
+
+    Parameters
+    ----------
+    xml_path : path to the ICDAR2019 xml annotation.
+
+    Returns
+    -------
+    tables : a list of :ref:`Table` objects from the xml annotation.
+    """
+    
+    from xml.dom import minidom
+    xmldoc = minidom.parse(xml_path)
+    
+    table_objs = []
+
+    tablelist = xmldoc.getElementsByTagName('table')
+
+    for table in tablelist:
+        table_obj = Table()
+
+        table_id = table.attributes['id'].value
+        table_obj.id = table_id
+
+        coords = [node for node in table.childNodes 
+                  if node.nodeType == table.ELEMENT_NODE][0].attributes["points"].value
+        coords = tuple([tuple(map(int, point.split(','))) 
+                        for point in coords.split()])
+        table_obj.coords = coords
+
+        for cell in table.getElementsByTagName("cell"):
+            cell_obj = Cell()
+            cell_id = cell.attributes['id'].value
+            cell_obj.id = cell_id
+
+            #start_row, end_row, start_col, end_col
+            location = \
+                cell.attributes["start-row"].value, \
+                cell.attributes["end-row"].value, \
+                cell.attributes["start-col"].value, \
+                cell.attributes["end-col"].value
+            location = tuple(map(int, location))
+            cell_obj.location = location
+
+            ccoords = [node for node in cell.childNodes 
+                      if node.nodeType == cell.ELEMENT_NODE][0].attributes["points"].value
+            ccoords = tuple([tuple(map(int, point.split(','))) 
+                             for point in ccoords.split()])
+            
+            cell_obj.coords = ccoords
+
+            table_obj.cells.append(cell_obj)
+        table_objs.append(table_obj)
+        
+    return table_objs
